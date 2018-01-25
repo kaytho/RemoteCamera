6852a7b6a26103c94353238a57a8d98c5d9e466c
aaronjamt <aaronjamt@gmail.com>
2018-01-25 15:42:39 -0600

    Update

--- app/src/main/java/net/aaronjamt/remotecamera/Main.java
+++ app/src/main/java/net/aaronjamt/remotecamera/Main.java
@@ -1,12 +1,16 @@
 package net.aaronjamt.remotecamera;
 
 import android.content.Context;
+import android.graphics.drawable.Icon;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.PointerIcon;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -25,12 +29,14 @@ public class Main extends AppCompatActivity {
     ArrayList<String> camerasAvalible=new ArrayList<>();
     android.net.wifi.WifiManager.MulticastLock lock;
     private String type = "_remoteCam._tcp.local.";
+    Boolean serverState = false;
 
     private void setUp() {
         try {
             WifiManager wifi =
                     (WifiManager)
                             getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+            assert wifi != null;
             lock = wifi.createMulticastLock("HeeereDnssdLock");
             lock.setReferenceCounted(true);
             lock.acquire();
@@ -50,7 +56,7 @@ public class Main extends AppCompatActivity {
                     while (itr.hasNext())
                     {
                         String x = (String)itr.next();
-                        if (x == ev.getName()) itr.remove(); break;
+                        if (x.equals(ev.getName())) itr.remove(); break;
                     }
                 }
 
@@ -74,10 +80,23 @@ public class Main extends AppCompatActivity {
         setupViewPager(mViewPager);
     }
 
-    private void setupViewPager(ViewPager viewPager) {
+    void setupViewPager(ViewPager viewPager) {
         SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
         adapter.addFragment(new tab1_client(), "Client");
         adapter.addFragment(new tab2_server(), "Server");
         viewPager.setAdapter(adapter);
+    }
+
+    public void onToggleClicked(View view) {
+        ImageButton button = view.findViewById(R.id.imageButton);
+        int icon;
+        if (serverState) {
+            serverState = false;
+            icon = R.drawable.off;
+        } else {
+            serverState = true;
+            icon = R.drawable.on;
+        }
+        button.setBackgroundResource(icon);
     }
 }
